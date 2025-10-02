e4ca74f29e1433a1f6d5fe73ccfd7315e67f410c
Techjar <tecknojar@gmail.com>
2025-10-02 01:59:45 -0400

    Add setting to filter forces
    Compatibility for blueprint sandboxes and other mods which cause duplicate research logging

--- control.lua
+++ control.lua
@@ -10,9 +10,19 @@ local function log_message(msg)
     )
 end
 
+local function check_force(force)
+    for name in string.gmatch(settings.global["fgcb-allowed-forces"].value, "([^,]+)") do
+        if force.name == name then
+            return true
+        end
+    end
+    return false
+end
+
 script.on_event(
     defines.events.on_research_finished,
     function(event)
+        if not check_force(event.research.force) then return end
         local research_name = event.research.name
         log_message("[RESEARCH_FINISHED:" .. research_name .. "]")
     end
@@ -21,6 +31,7 @@ script.on_event(
 script.on_event(
     defines.events.on_research_started,
     function(event)
+        if not check_force(event.research.force) then return end
         local research_name = event.research.name
         log_message("[RESEARCH_STARTED:" .. research_name .. "]")
     end
@@ -29,6 +40,7 @@ script.on_event(
 script.on_event(
     defines.events.on_rocket_launched,
     function(event)
+        if not check_force(event.rocket.force) then return end
         storage.rocketLaunched = storage.rocketLaunched or 0
         storage.rocketLaunched = storage.rocketLaunched + 1
         log_message("[ROCKET_LAUNCHED:" .. storage.rocketLaunched .. "]")
@@ -51,8 +63,8 @@ end
 script.on_event(
     defines.events.on_pre_player_died,
     function(event)
-        local player_index = event.player_index
-        local player_name = game.get_player(player_index).name
+        local player = game.get_player(event.player_index)
+        if not check_force(player.force) then return end
         local cause = event.cause
         local causeText = ""
         if event.cause then
@@ -63,9 +75,9 @@ script.on_event(
             causeText = event.cause.player.name
         end
 
-        local count, total = getAndStoreDeathCount(player_name, causeText)
+        local count, total = getAndStoreDeathCount(player.name, causeText)
 
-        log_message("[PLAYER_DIED:" .. player_name .. ":" .. causeText .. ":" .. count .. ":" .. total .. "]")
+        log_message("[PLAYER_DIED:" .. player.name .. ":" .. causeText .. ":" .. count .. ":" .. total .. "]")
     end
 )
 
---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+     type = "string-setting",
+     name = "fgcb-allowed-forces",
+     setting_type = "runtime-global",
+     default_value = "player",
+     allow_blank = false,
+     auto_trim = true
+    }
+})
